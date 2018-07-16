a0fd39df130c7aa70ebcec9c71e2257b1f0ce9b7
Tobiasz18 <tobiaszpost@gmail.com>
2018-07-16 14:32:37 +0200

    test express

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
+let profileObject = {};
+const config = require('./config.js');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('hellow all right')
+});
+
+app.listen(3000);
