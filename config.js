77cad3a375781586a52d8f85415a73e0aea93824
Tobiasz18 <tobiaszpost@gmail.com>
2018-07-16 18:12:16 +0200

    facebook config and endpoints

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'clientID': '206611430042831',
+    'clientSecret': '271969fe7b023adcce5ccdd79c9403a4',
+    'callbackURL': 'https://localhost:3000/auth/facebook/callback'
+}
--- script.js
+++ script.js
@@ -5,9 +5,45 @@ let profileObject = {};
 const config = require('./config.js');
 const app = express();
 
+passport.serializeUser(function(user, done) {
+  return done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  return done(null, obj);
+});
+
+passport.use(new FacebookStrategy({
+        clientID: config.clientID,
+        clientSecret: config.clientSecret,
+        callbackURL: config.callbackURL
+    },
+    function(accessToken, refreshToken, profile, done) {
+        profileObject = {
+            profile: profile
+        }
+        done(null, profile)
+    }
+));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
-    res.send('hellow all right')
+    res.render('home')
 });
+
+app.get('/login', (req, res) => {
+    res.render('logged')
+})
+
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'read_stream' }));
+
+app.get('auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}))
 
 app.listen(3000);
